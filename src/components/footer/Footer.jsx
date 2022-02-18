6d91ff300eee5ae2ff166934aca84399bd5dec87
Ionel Florin Avram <avram.ionel.florin@gmail.com>
2022-02-18 23:46:28 +0200

    instaled fontAwsome and finished the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { Navbar, Container } from "react-bootstrap";
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <Navbar expand="lg" variant="dark" bg="dark" fixed="bottom">
-      <Container>
+      <Container fluid>
         <Navbar.Brand>
-          App created using {" "}
-          &nbsp;<a href="https://opentdb.com/" className="header-small-info">
+          App created using &nbsp;
+          <a href="https://opentdb.com/" className="header-small-info">
             {""}
-             Open Trivia Database
+            Open Trivia Database
           </a>
         </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Navbar.Brand href="https://github.com/AvramIonel/trivia-otd.git">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </Navbar.Brand>
+          <Navbar.Brand href="https://www.linkedin.com/in/ionel-florin-avram-94a2715b">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </Navbar.Brand>
+        </Nav>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
