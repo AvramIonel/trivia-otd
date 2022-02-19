bc00436a5601f5483a6ef2ef235980bdeaeb3a31
Ionel Florin Avram <avram.ionel.florin@gmail.com>
2022-02-20 00:25:22 +0200

    solved the footer problem for phone us

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,28 +3,26 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <Navbar expand="lg" variant="dark" bg="dark" fixed="bottom">
-      <Container fluid>
-        <Navbar.Brand>
-          App created using &nbsp;
-          <a href="https://opentdb.com/" className="header-small-info">
-            {""}
-            Open Trivia Database
-          </a>
-        </Navbar.Brand>
-        <Nav className="mr-auto">
+    <Container fluid>
+      <Navbar expand="md" variant="dark" bg="dark" fixed="bottom">
+        <Nav className="icons-position">
+          <Navbar.Brand href="https://opentdb.com/">
+            <FontAwesomeIcon icon={faDatabase} size="1x" />
+          </Navbar.Brand>
           <Navbar.Brand href="https://github.com/AvramIonel/trivia-otd.git">
-            <FontAwesomeIcon icon={faGithub} size="2x" />
+            <FontAwesomeIcon icon={faGithub} size="1x" />
           </Navbar.Brand>
           <Navbar.Brand href="https://www.linkedin.com/in/ionel-florin-avram-94a2715b">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <FontAwesomeIcon icon={faLinkedin} size="1x" />
           </Navbar.Brand>
         </Nav>
-      </Container>
-    </Navbar>
+      </Navbar>
+    </Container>
   );
 };
 
